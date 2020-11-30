622afa5beec8d46f93c9f15bed32d81a48eda47e
Pontus Strömberg <pontus.stromberg@hotmail.se>
2020-11-30 16:07:46 +0100

    updated folders

--- exercism/c/crypto-square/src/crypto_square.h
+++
@@ -1,8 +0,0 @@
-#ifndef CRYPTO_SQUARE_H
-#define CRYPTO_SQUARE_H
-
-#define MAX_LEN 100
-
-char *ciphertext(const char *input);
-
-#endif
--- exercism/c/react/src/react.h
+++
@@ -1,6 +0,0 @@
-#ifndef REACT_H
-#define REACT_H
-
-
-
-#endif
